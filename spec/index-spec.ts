468652d2626700cae717d655a28c666f3da16cf3
smhxx <captaintrek@gmail.com>
2017-10-06 20:04:58 -0500

    test(*): add tests to reach 100% test coverage
    - Tweaking fixtures in preparation for additional tests
    - Major refactoring to test suite
    - Add tests for edge cases: Missing or invalid tsconfig file
    - Add test for edge case: config has no compilerOptions
    - Adding test for edge case: No local typescript package installed
    - Adding test for edge case: Requested source file can't be read
    - Adding test for edge case: transpile() called but no transpiler is installed
    
    Closes #10

--- spec/index-spec.ts
+++ spec/index-spec.ts
@@ -1,82 +1,113 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import { getCacheKeyData, transpile } from '../src/index';
+import { Fixtures } from './fixtures/fixtures';
+import { Options, PackageMeta, TsConfig } from '../src/defs';
 
 describe('atom-ts-transpiler', () => {
-  const filePath1 = path.resolve('spec/fixtures/index.ts');
-  const filePath2 = path.resolve('spec/fixtures/some/deep/directory/structure/index.ts');
-  const fileContents1 = fs.readFileSync(filePath1).toString();
-  const fileContents2 = fs.readFileSync(filePath2).toString();
 
   describe('getCacheKeyData()', () => {
-    const pkgData = fs.readFileSync(path.resolve('spec/fixtures/package.json'));
-    const pkg = {
-      meta: JSON.parse(pkgData.toString()),
-      name: 'mock-typescript',
-      path: path.resolve('spec/fixtures'),
-    };
-    const config = {
-      glob: '**/*.{ts,tsx}',
-      transpiler: '',
-    };
-    const cacheKeyFiles = [
-      'index.ts',
-      'some/deep/directory/structure/index.ts',
-    ];
 
-    it('returns empty string if no cacheKeyFiles are specified', () => {
-      const data: string = getCacheKeyData('', '', config, pkg);
-      expect(data).toEqual('');
-    });
+    const meta:PackageMeta = {
+      name: 'good-package',
+      path: Fixtures.goodPackage.package.directory,
+      meta: Fixtures.goodPackage.package.json,
+    };
 
     it('returns the concatenated contents of the listed cacheKeyFiles', () => {
-      const configWithFiles = Object.assign({}, config, { cacheKeyFiles });
-      const data: string = getCacheKeyData('', '', configWithFiles, pkg);
+      const fixture = Fixtures.goodPackage;
+      const cacheKeyFiles = [
+        'index.ts',
+        'other.ts',
+      ];
+      const config = Object.assign({}, fixture.config.json, { cacheKeyFiles });
+      const data: string = getCacheKeyData('', '', config, meta);
       expect(data).not.toEqual('');
-      expect(data).toMatch(fileContents1);
-      expect(data).toMatch(fileContents2);
+      expect(data).toMatch(fixture.index.contents);
+      expect(data).toMatch(fixture.other.contents);
+    });
+
+    it('returns empty string if no cacheKeyFiles are specified', () => {
+      const fixture = Fixtures.goodPackage;
+      const data: string = getCacheKeyData('', '', fixture.config.json, meta);
+      expect(data).toEqual('');
     });
   });
 
   describe('transpile()', () => {
-    const mock = require('./fixtures/node_modules/typescript/mock.ts');
-    const transpileModule = jest.spyOn(mock, 'transpileModule');
 
-    afterEach(() => {
-      transpileModule.mockReset();
+    const fixture = Fixtures.goodPackage;
+    const spy = fixture.typescript.module.transpileModule = jest.fn();
+
+    beforeEach(() => {
+      spy.mockReset();
+      spy.mockImplementation(() => ({
+        diagnostics: [],
+        outputText: 'output',
+      }));
     });
 
     afterAll(() => {
-      transpileModule.mockRestore();
+      delete fixture.typescript.module.transpileModule;
     });
 
     it('calls the appropriate transpiler with the contents of the source file', () => {
-      transpile('', filePath1);
-      expect(transpileModule).toHaveBeenCalledTimes(1);
-      const source = transpileModule.mock.calls[0][0];
-      expect(source).toEqual(fileContents1);
+      transpile('', fixture.index.path, {});
+      expect(spy).toHaveBeenCalledTimes(1);
+      const source = spy.mock.calls[0][0];
+      expect(source).toEqual(fixture.index.contents);
     });
 
     it('writes any encountered errors to console.error() and does not return any code', () => {
-      const spy = jest.spyOn(console, 'error').mockImplementation(() => undefined);
-      const output = transpile('', filePath1);
-      expect(spy).toHaveBeenCalledTimes(1);
+      const error = jest.spyOn(console, 'error');
+      error.mockImplementation(() => void 0);
+      spy.mockImplementationOnce(() => {
+        throw new Error('the message attached to the thrown error');
+      });
+
+      const output = transpile('', fixture.index.path, {});
+      expect(error).toHaveBeenCalledTimes(1);
+      expect(typeof error.mock.calls[0][0]).toBe('string');
+      expect(error.mock.calls[0][0]).toMatch(/the message attached to the thrown error/);
       expect(typeof output).toBe('object');
       expect(output.code).toBeUndefined();
+
+      error.mockRestore();
     });
 
     it('uses the compiler options specified in tsconfig.json', () => {
-      transpile('', filePath1);
-      expect(transpileModule).toHaveBeenCalledTimes(1);
-      const options = transpileModule.mock.calls[0][1].compilerOptions;
-      expect(options.removeComments).toBe(true);
+      transpile('', fixture.index.path, {});
+      expect(spy).toHaveBeenCalledTimes(1);
+      const usedOptions = spy.mock.calls[0][1].compilerOptions;
+      expect(usedOptions.removeComments).toBe(true);
     });
 
     it('overrides the tsconfig options with any specified in the package.json', () => {
-      transpile('', filePath1, { removeComments: false });
-      expect(transpileModule).toHaveBeenCalledTimes(1);
-      const options = transpileModule.mock.calls[0][1].compilerOptions;
-      expect(options.removeComments).toBe(false);
+      const compilerOptions: TsConfig.CompilerOptions = { removeComments: false };
+      const options: Options = { compilerOptions };
+      transpile('', fixture.index.path, options);
+      expect(spy).toHaveBeenCalledTimes(1);
+      const usedOptions = spy.mock.calls[0][1].compilerOptions;
+      expect(usedOptions.removeComments).toBe(false);
+    });
+
+    it('writes an error to console and returns no code if the file does not exist', () => {
+      const error = jest.spyOn(console, 'error');
+      error.mockImplementation(() => void 0);
+      const filePath = path.resolve(fixture.index.directory, 'not-a-real-file.ts');
+      const output = transpile('', filePath, {});
+      expect(error).toHaveBeenCalledTimes(1);
+      expect(error.mock.calls[0][0]).toMatch(/ENOENT/);
+      expect(output).toEqual({});
+      error.mockRestore();
+    });
+
+    it('returns no code if the typescript package could not be resolved', () => {
+      const error = jest.spyOn(console, 'error');
+      error.mockImplementation(() => void 0);
+      const output = transpile('', Fixtures.noTranspilerPackage.index.path, {});
+      expect(error).toHaveBeenCalledTimes(1);
+      expect(output).toEqual({});
+      error.mockRestore();
     });
   });
 });
