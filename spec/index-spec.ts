ea5b7226eac24fa3bce02e007a43e60498472203
smhxx <captaintrek@gmail.com>
2017-10-08 18:02:23 -0500

    Feature: Add new option for verbose console output (#11)
    * Tweaking fixture loader in preparation for new tests
    * Adding verbose output option and associated tests

--- spec/index-spec.ts
+++ spec/index-spec.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
+import { fixtures } from './fixtures/fixtures';
 import { getCacheKeyData, transpile } from '../src/index';
-import { Fixtures } from './fixtures/fixtures';
 import { Options, PackageMeta, TsConfig } from '../src/defs';
 
 describe('atom-ts-transpiler', () => {
@@ -9,12 +9,12 @@ describe('atom-ts-transpiler', () => {
 
     const meta:PackageMeta = {
       name: 'good-package',
-      path: Fixtures.goodPackage.package.directory,
-      meta: Fixtures.goodPackage.package.json,
+      path: fixtures.goodPackage.package.directory,
+      meta: fixtures.goodPackage.package.json,
     };
 
     it('returns the concatenated contents of the listed cacheKeyFiles', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const cacheKeyFiles = [
         'index.ts',
         'other.ts',
@@ -27,7 +27,7 @@ describe('atom-ts-transpiler', () => {
     });
 
     it('returns empty string if no cacheKeyFiles are specified', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const data: string = getCacheKeyData('', '', fixture.config.json, meta);
       expect(data).toEqual('');
     });
@@ -35,26 +35,25 @@ describe('atom-ts-transpiler', () => {
 
   describe('transpile()', () => {
 
-    const fixture = Fixtures.goodPackage;
-    const spy = fixture.typescript.module.transpileModule = jest.fn();
+    let spy: jest.Mock<{}>;
 
     beforeEach(() => {
-      spy.mockReset();
+      spy = fixtures.goodPackage.typescript.module.transpileModule = jest.fn();
       spy.mockImplementation(() => ({
         diagnostics: [],
         outputText: 'output',
       }));
     });
 
-    afterAll(() => {
-      delete fixture.typescript.module.transpileModule;
+    afterEach(() => {
+      jest.restoreAllMocks();
     });
 
     it('calls the appropriate transpiler with the contents of the source file', () => {
-      transpile('', fixture.index.path, {});
+      transpile('', fixtures.goodPackage.index.path, {});
       expect(spy).toHaveBeenCalledTimes(1);
       const source = spy.mock.calls[0][0];
-      expect(source).toEqual(fixture.index.contents);
+      expect(source).toEqual(fixtures.goodPackage.index.contents);
     });
 
     it('writes any encountered errors to console.error() and does not return any code', () => {
@@ -64,18 +63,16 @@ describe('atom-ts-transpiler', () => {
         throw new Error('the message attached to the thrown error');
       });
 
-      const output = transpile('', fixture.index.path, {});
+      const output = transpile('', fixtures.goodPackage.index.path, {});
       expect(error).toHaveBeenCalledTimes(1);
       expect(typeof error.mock.calls[0][0]).toBe('string');
       expect(error.mock.calls[0][0]).toMatch(/the message attached to the thrown error/);
       expect(typeof output).toBe('object');
       expect(output.code).toBeUndefined();
-
-      error.mockRestore();
     });
 
     it('uses the compiler options specified in tsconfig.json', () => {
-      transpile('', fixture.index.path, {});
+      transpile('', fixtures.goodPackage.index.path, {});
       expect(spy).toHaveBeenCalledTimes(1);
       const usedOptions = spy.mock.calls[0][1].compilerOptions;
       expect(usedOptions.removeComments).toBe(true);
@@ -84,7 +81,7 @@ describe('atom-ts-transpiler', () => {
     it('overrides the tsconfig options with any specified in the package.json', () => {
       const compilerOptions: TsConfig.CompilerOptions = { removeComments: false };
       const options: Options = { compilerOptions };
-      transpile('', fixture.index.path, options);
+      transpile('', fixtures.goodPackage.index.path, options);
       expect(spy).toHaveBeenCalledTimes(1);
       const usedOptions = spy.mock.calls[0][1].compilerOptions;
       expect(usedOptions.removeComments).toBe(false);
@@ -93,7 +90,7 @@ describe('atom-ts-transpiler', () => {
     it('writes an error to console and returns no code if the file does not exist', () => {
       const error = jest.spyOn(console, 'error');
       error.mockImplementation(() => void 0);
-      const filePath = path.resolve(fixture.index.directory, 'not-a-real-file.ts');
+      const filePath = path.resolve(fixtures.goodPackage.index.directory, 'not-a-real-file.ts');
       const output = transpile('', filePath, {});
       expect(error).toHaveBeenCalledTimes(1);
       expect(error.mock.calls[0][0]).toMatch(/ENOENT/);
@@ -104,10 +101,26 @@ describe('atom-ts-transpiler', () => {
     it('returns no code if the typescript package could not be resolved', () => {
       const error = jest.spyOn(console, 'error');
       error.mockImplementation(() => void 0);
-      const output = transpile('', Fixtures.noTranspilerPackage.index.path, {});
+      const output = transpile('', fixtures.noTranspilerPackage.index.path, {});
       expect(error).toHaveBeenCalledTimes(1);
       expect(output).toEqual({});
       error.mockRestore();
+    });
+
+    it('writes debugging text to console if the verbose option is set to true', () => {
+      const log = jest.spyOn(console, 'log');
+      log.mockImplementation(() => void 0);
+      transpile('', fixtures.goodPackage.index.path, { verbose: true });
+      expect(log).toHaveBeenCalledTimes(2);
+      log.mockRestore();
+    });
+
+    it('does not write to the console if the verbose option is not set', () => {
+      const log = jest.spyOn(console, 'log');
+      log.mockImplementation(() => void 0);
+      transpile('', fixtures.goodPackage.index.path, {});
+      expect(log).not.toHaveBeenCalled();
+      log.mockRestore();
     });
   });
 });
--- spec/resolve-spec.ts
+++ spec/resolve-spec.ts
@@ -1,26 +1,26 @@
 import { resolveConfig, resolveTranspiler } from '../src/resolve';
-import { Fixtures } from './fixtures/fixtures';
+import { fixtures } from './fixtures/fixtures';
 
 describe('resolve.ts', () => {
 
   describe('resolveOptions()', () => {
 
     it('returns the contents of the project\'s tsconfig file', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const resolved = resolveConfig(fixture.index.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toEqual(fixture.config.json.compilerOptions);
     });
 
     it('traverses the directory structure to search in parent directories', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const resolved = resolveConfig(fixture.deep.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toEqual(fixture.config.json.compilerOptions);
     });
 
     it('returns {} if the config file cannot be resolved', () => {
-      const fixture = Fixtures.noConfigPackage;
+      const fixture = fixtures.noConfigPackage;
       const resolved = resolveConfig(fixture.index.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toEqual({});
@@ -29,7 +29,7 @@ describe('resolve.ts', () => {
     it('writes an error to console and returns {} if the resolved config file is invalid', () => {
       const error = jest.spyOn(console, 'error');
       error.mockImplementation(() => void 0);
-      const fixture = Fixtures.badConfigPackage;
+      const fixture = fixtures.badConfigPackage;
       const resolved = resolveConfig(fixture.index.directory);
       expect(error).toHaveBeenCalledTimes(1);
       expect(resolved).toBeDefined();
@@ -38,7 +38,7 @@ describe('resolve.ts', () => {
     });
 
     it('returns {} if the config\'s compilerOptions do not exist or are not an object', () => {
-      const fixture = Fixtures.noOptionsPackage;
+      const fixture = fixtures.noOptionsPackage;
       const resolved = resolveConfig(fixture.index.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toEqual({});
@@ -48,14 +48,14 @@ describe('resolve.ts', () => {
   describe('resolveTranspiler()', () => {
 
     it('returns the project\'s local TypeScript transpiler module', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const resolved = resolveTranspiler(fixture.index.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toBe(fixture.typescript.module);
     });
 
     it('traverses the directory structure to search in parent directories', () => {
-      const fixture = Fixtures.goodPackage;
+      const fixture = fixtures.goodPackage;
       const resolved = resolveTranspiler(fixture.deep.directory);
       expect(resolved).toBeDefined();
       expect(resolved).toBe(fixture.typescript.module);
@@ -64,7 +64,7 @@ describe('resolve.ts', () => {
     it('writes an error to console and returns undefined if there is no typescript package', () => {
       const error = jest.spyOn(console, 'error');
       error.mockImplementation(() => void 0);
-      const fixture = Fixtures.noTranspilerPackage;
+      const fixture = fixtures.noTranspilerPackage;
       const resolved = resolveTranspiler(fixture.index.directory);
       expect(resolved).toBeUndefined();
       error.mockRestore();
