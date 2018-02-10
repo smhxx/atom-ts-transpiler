944878f1034650d45de3d50a81380b312f8e17ad
smhxx <captaintrek@gmail.com>
2018-02-10 17:55:10 -0600

    chore(examples): refactor exports for example code

--- examples/simple/lib/index.ts
+++ examples/simple/lib/index.ts
@@ -9,44 +9,40 @@ interface ISerializedState {
   myAtomPackageViewState?: object;
 }
 
-export default {
+export function activate(state: ISerializedState): void {
+  myAtomPackageView = new MyAtomPackageView(state.myAtomPackageViewState);
+  modalPanel = atom.workspace.addModalPanel({
+    item: myAtomPackageView.getElement(),
+    visible: false,
+  });
 
-  activate(state: ISerializedState): void {
-    myAtomPackageView = new MyAtomPackageView(state.myAtomPackageViewState);
-    modalPanel = atom.workspace.addModalPanel({
-      item: myAtomPackageView.getElement(),
-      visible: false,
-    });
+  // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
+  subscriptions = new CompositeDisposable();
 
-    // Events subscribed to in atom's system can be easily cleaned up with a CompositeDisposable
-    subscriptions = new CompositeDisposable();
+  // Register command that toggles this view
+  subscriptions.add(atom.commands.add("atom-workspace", {
+    "my-atom-package:toggle": () => toggle(),
+  }));
+}
 
-    // Register command that toggles this view
-    subscriptions.add(atom.commands.add("atom-workspace", {
-      "my-atom-package:toggle": () => this.toggle(),
-    }));
-  },
+export function deactivate(): void {
+  modalPanel.destroy();
+  subscriptions.dispose();
+  myAtomPackageView.destroy();
+}
 
-  deactivate(): void {
-    modalPanel.destroy();
-    subscriptions.dispose();
-    myAtomPackageView.destroy();
-  },
+export function serialize(): ISerializedState {
+  return {
+    myAtomPackageViewState: myAtomPackageView.serialize(),
+  };
+}
 
-  serialize(): ISerializedState {
-    return {
-      myAtomPackageViewState: myAtomPackageView.serialize(),
-    };
-  },
-
-  toggle(): void {
-    // tslint:disable-next-line no-console
-    console.log("MyAtomPackage was toggled!");
-    return (
-      modalPanel.isVisible() ?
-      modalPanel.hide() :
-      modalPanel.show()
-    );
-  },
-
-};
+function toggle(): void {
+  // tslint:disable-next-line no-console
+  console.log("MyAtomPackage was toggled!");
+  return (
+    modalPanel.isVisible() ?
+    modalPanel.hide() :
+    modalPanel.show()
+  );
+}
